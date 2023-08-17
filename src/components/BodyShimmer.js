15226ca8f95672c63a2a42d27bc4173808337143
Ajay481 <iamajayyadav481@gmail.com>
2023-08-17 12:40:58 +0530

    BodyShimmer Optimised

--- src/components/BodyShimmer.js
+++ src/components/BodyShimmer.js
@@ -3,19 +3,19 @@ import React from "react";
 const BodyShimmer = () => {
   return (
     <div className="flex flex-wrap justify-between h-[100vh]">
-      {Array(5)
+      {Array(4)
         .fill("")
         .map((e, index) => (
           <div
-            className="bg-gray-300 w-72 h-72 shadow-lg m-4 rounded-lg"
+            className="bg-gray-300 w-72 h-52 shadow-lg m-20 rounded-lg"
             key={index}
           ></div>
         ))}
-      {Array(20)
+      {Array(16)
         .fill("")
         .map((e, index) => (
           <div
-            className="bg-gray-300 w-72 h-36 shadow-lg m-4 mt-16 rounded-lg"
+            className="bg-gray-300 w-72 h-36 shadow-lg m-20 mt-8 rounded-lg"
             key={index}
           >
             <ul className="mt-40">
